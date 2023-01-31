ecad64757f17962f0e964808f12c283edfc1eab8
Ananya59 <89981752+Ananya59@users.noreply.github.com>
2023-01-31 18:40:07 +0530

    Add files via upload

--- game_login.js
+++ game_login.js
@@ -1,10 +1,9 @@
-function addUser()
-{
+function addUser() {
     player1_name = document.getElementById("player1_name").value;
-    player2_name = document.getElementById("player2_name").value;
+    player2_name =document.getElementById("player2_name").value;
 
     localStorage.setItem("player1_name" , player1_name);
     localStorage.setItem("player2_name" , player2_name);
 
-    window.location.replace("game_page.html");
+    window.location = "game_page.html";
 }
